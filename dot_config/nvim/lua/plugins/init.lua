155f5dfe3cb96640bc6fe9247d4bcf59a581ce32
Dan Pickett <dan.pickett@launchware.com>
2024-03-31 19:53:05 -0400

    add trouble.vim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -28,6 +28,15 @@ lazy.setup({
     end
   },
   {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
     "zaldih/themery.nvim",
     opts = require("plugins.configs.themery")
   },
