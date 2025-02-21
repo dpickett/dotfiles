279e76cca8680a9a1dfafaf81b54099c5f99669a
Dan Pickett <dan.pickett@launchware.com>
2025-02-21 00:42:13 -0500

    update support for mac os x configuration

--- dot_antidote_plugins/mise/mise.plugin.zsh
+++ dot_antidote_plugins/mise/mise.plugin.zsh
@@ -1,6 +1,4 @@
 #!/bin/env zsh
 
 export MISE_OVERRIDE_TOOL_VERSIONS_FILENAME=none
-
 eval "$(~/bin/mise activate zsh)"
-~/bin/mise settings override_tool_versions_filename=none
--- dot_antidote_plugins/mise/run_once_install_mise.sh
+++ dot_antidote_plugins/mise/run_once_install_mise.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-[[ ! -f ~/bin/mise ]] || curl https://mise.run | MISE_INSTALL_PATH=~/bin/mise sh
+[[ -f ~/bin/mise ]] || curl https://mise.run | MISE_INSTALL_PATH=~/bin/mise sh
